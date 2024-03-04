5528512bfde6d7469070009ceb6da4282bb78fa0
ashitaverma07 <ashitaverma14@gmail.com>
2024-03-04 23:38:39 +0530

    func

--- script.js
+++ script.js
@@ -2,12 +2,26 @@ const apiKey = 'd95d490989384d94a4c489244c84e788';
 
 const blogContainer = document.getElementById("blog-container");
 
-function fetchRandom() {
+async function fetchRandomNews() {
     try {
         const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=${apiKey}`;
+        const response = await fetch(apiUrl)
+        const data = await response.json()
+        console.log(data);
     }
     catch (error) {
         console.error("Error fetching random news", error)
         return []
     }
 }
+
+
+
+async () => {
+    try {
+        const articles = await fetchRandomNews();
+        console.log(articles);
+    } catch (error) {
+        console.error("Error fetching random news", error);
+    }
+};
