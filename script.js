4b6c0583d112a0e763e48a0bddd075e4eb81b576
ashitaverma07 <ashitaverma14@gmail.com>
2024-03-04 23:21:56 +0530

    links

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const apiKey = 'd95d490989384d94a4c489244c84e788';
 
 const blogContainer = document.getElementById("blog-container");
 
-function fetchRandom(){
-    
+function fetchRandom() {
+    try {
+        
+    }
+    catch (error) {
+        console.error("Error fetching random news", error)
+        return []
+    }
 }
