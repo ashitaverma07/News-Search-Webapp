89095f80f09f05272324d83b19a39b50990ec6f9
ashitaverma07 <ashitaverma14@gmail.com>
2024-03-04 23:26:28 +0530

    url

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const blogContainer = document.getElementById("blog-container");
 
 function fetchRandom() {
     try {
-        
+        const apiUrl = `https://newsapi.org/v2/top-headlines?country=us`
     }
     catch (error) {
         console.error("Error fetching random news", error)
