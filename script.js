10f9a87fca63dd323b9a04311e16582432c2c510
ashitaverma07 <ashitaverma14@gmail.com>
2024-03-04 23:18:05 +0530

    api link

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const apiKey = 'd95d490989384d94a4c489244c84e788'
+const apiKey = 'd95d490989384d94a4c489244c84e788';
+
+const blogContainer = document.getElementById("blog-container");
+
+function fetchRandom(){
+    
+}
